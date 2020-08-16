4eaba8fd36a096ba32d267978c51f2d3591d2afa
Joseph-D98 <defelicejoseph@outlook.com>
2020-08-16 10:09:47 -0600

    character-array/pass-length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
-// Assignment code here
+// character arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-// Get references to the #generate element
+// prompt for desired password length, parameters: 8-128
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password length must be between 8-128 characters Try again");
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+    } 
+
+    // display password length  
+    alert(`Your password will have ${confirmLength} characters`);
+
+    
+
+// get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +38,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
